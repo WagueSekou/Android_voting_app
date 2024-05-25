c8045135ee2b13a46dc66adb0e347a6a01acb908
waguesekou <waguesekounick@gmail.com>
2024-05-25 23:39:57 +0100

    addition of admin homepage && candidate page

--- app/src/main/java/com/example/voting_system/Condidate_creationActivity.java
+++ app/src/main/java/com/example/voting_system/Condidate_creationActivity.java
@@ -1,7 +1,6 @@
-package com.example.voting_system.activities;
+package com.example.voting_system;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,20 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.voting_system.R;
-
-import de.hdodenhof.circleimageview.CircleImageView;
-
-public class HomePageActivity extends AppCompatActivity {
- private CircleImageView circleImg;
- private TextView name, nationalId;
- private String uid;
+public class Condidate_creationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_candidate_creation);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
