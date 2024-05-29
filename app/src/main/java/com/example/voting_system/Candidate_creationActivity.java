9d1654c622b455e38aea372fe6717bbde2dfaf46
Ryane23 <erickryan2@gmail.com>
2024-05-29 22:04:55 +0100

    candidate page complited

--- app/src/main/java/com/example/voting_system/Candidate_creationActivity.java
+++ app/src/main/java/com/example/voting_system/Candidate_creationActivity.java
@@ -1,6 +1,9 @@
 package com.example.voting_system;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Condidate_creationActivity extends AppCompatActivity {
 
+    private CircleImageView candidateImg;
+    private EditText candidateName, candidateParty;
+    private Spinner candidateSpinner;
+    private Button submitBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_candidate_creation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
